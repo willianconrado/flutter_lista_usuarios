62a7c4f8b5529d0c8abc99d7c34c44d35be0a28b
Willian Conrado <willian.conrado@gmail.com>
2023-04-02 21:03:25 -0300

    alterações

---
+++ lib/data/dummy_user.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_crud/models/user.dart';
+
+const dummyUsers = {
+  '1': User(
+    id: '1',
+    name: 'Willian Conrado',
+    email: 'willian.conrado@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/11/01/21/11/avatar-1789663_960_720.png',
+  ),
+  '2': User(
+    id: '2',
+    name: 'Guilherme Conrado',
+    email: 'joao.gui.conrado@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635464_960_720.png',
+  ),
+  '3': User(
+    id: '3',
+    name: 'Cicilia Conrado',
+    email: 'cici.conrado@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2017/01/31/19/07/avatar-2026510_960_720.png',
+  ),
+  '4': User(
+    id: '4',
+    name: 'teste',
+    email: 'teste@gmail.com',
+    avatarUrl: '',
+  ),
+};
